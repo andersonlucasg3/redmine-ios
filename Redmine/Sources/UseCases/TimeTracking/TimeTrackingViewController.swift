a39913c8aa7b662d13cc172876b46938d4797b77
Anderson Lucas C. Ramos <anderson@insanitech.com.br>
2018-05-11 16:50:40 -0300

    Adicionando tela de TimeTracking;

---
+++ Redmine/Sources/UseCases/TimeTracking/TimeTrackingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TimeTrackingViewController.swift
+//  Redmine
+//
+//  Created by Anderson Lucas C. Ramos on 11/05/2018.
+//  Copyright © 2018 InsaniTech. All rights reserved.
+//
+
+import UIKit
+
+class TimeTrackingViewController: UITableViewController {
+    
+}
